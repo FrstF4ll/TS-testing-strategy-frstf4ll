a19c7b05b8919ca99433ea2cdaa012863e4d6ab2
FrstF4ll <207869736+FrstF4ll@users.noreply.github.com>
2026-09-25 14:22:06 +0200

    fix: script and playwright config with new scripts.

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,5 +5,8 @@ const baseURL = 'http://localhost:5173'
 export default defineConfig({
     testDir: './e2e',
     use: { baseURL: baseURL },
-    webServer: { command: 'pnpm dev', url: baseURL, reuseExistingServer: true },
+webServer: [
+        { command: 'pnpm dev:server', url: 'http://localhost:3000/health', reuseExistingServer: true },
+        { command: 'pnpm dev', url: baseURL, reuseExistingServer: true },
+    ],
 })
