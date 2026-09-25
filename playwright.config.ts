10b70967504eab0560a19ae489290410dd62a144
FrstF4ll <207869736+FrstF4ll@users.noreply.github.com>
2026-09-25 12:10:25 +0200

    chore: build config files for testing frameworks

---
+++ playwright.config.ts
@@ -0,0 +1,9 @@
+import {defineConfig} from "@playwright/test";
+
+const baseURL = 'http://localhost:5173'
+
+defineConfig({
+    testDir: './e2e',
+    use: { baseURL: baseURL },
+    webServer: { command: 'pnpm dev', url: baseURL, reuseExistingServer: true },
+})
